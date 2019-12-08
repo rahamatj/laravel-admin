e699a824aeb62d164ef96f419220e64632a20cf3
unknown <rahamatj92@gmail.com>
2019-12-08 16:18:02 +0600

    auth middleware redirect fixed

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,19 +2,40 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
     /**
+     * Handle an unauthenticated user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  array  $guards
+     * @return void
+     *
+     * @throws \Illuminate\Auth\AuthenticationException
+     */
+    protected function unauthenticated($request, array $guards)
+    {
+        throw new AuthenticationException(
+            'Unauthenticated.', $guards, $this->redirectRoute($request, $guards)
+        );
+    }
+
+    /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
      * @param  \Illuminate\Http\Request  $request
+     * @param  array  $guards
      * @return string
      */
-    protected function redirectTo($request)
+    protected function redirectRoute($request, array $guards)
     {
         if (! $request->expectsJson()) {
+            if(in_array('admin', $guards))
+                return route('admin.login');
+
             return route('login');
         }
     }

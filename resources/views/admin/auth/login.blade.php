1cbad6baf1971321c69b801b304451343fa7636f
unknown <rahamatj92@gmail.com>
2019-12-08 11:58:14 +0600

    layout added for admin auth views

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,87 +1,60 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <!-- Meta, title, CSS, favicons, etc. -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('admin.auth.layout')
 
-    <title>{{ config('app.name') }} | Admin Login</title>
+@section('title', 'Admin Login')
 
-    <!-- Bootstrap -->
-    <link href="{{ asset('/assets/admin/vendors') }}/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Font Awesome -->
-    <link href="{{ asset('/assets/admin/vendors') }}/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <!-- NProgress -->
-    <link href="{{ asset('/assets/admin/vendors') }}/nprogress/nprogress.css" rel="stylesheet">
-    <!-- Animate.css -->
-    <link href="{{ asset('/assets/admin/vendors') }}/animate.css/animate.min.css" rel="stylesheet">
+@section('content')
+    <form method="POST" action="{{ route('admin.login') }}">
+        @csrf
+        <h1>Admin Login</h1>
+        <div>
+            <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email"
+                   name="email"
+                   value="{{ old('email') }}" required autocomplete="email" autofocus/>
 
-    <!-- Custom Theme Style -->
-    <link href="{{ asset('/assets/admin/build') }}/css/custom.min.css" rel="stylesheet">
-</head>
-
-<body class="login">
-<div>
-    <div class="login_wrapper">
-        <div class="animate form login_form">
-            <section class="login_content">
-                <form method="POST" action="{{ route('admin.login') }}">
-                    @csrf
-                    <h1>Admin Login</h1>
-                    <div>
-                        <input type="email" class="form-control @error('email') is-invalid @enderror" placeholder="Email" name="email"
-                               value="{{ old('email') }}" required autocomplete="email" autofocus/>
-
-                        @error('email')
-                        <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                    <div>
-                        <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password" name="password" required
-                               autocomplete="current-password"/>
-
-                        @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                    <div class="checkbox" style="text-align: left">
-                        <label>
-                            <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
-                        </label>
-                    </div>
-                    <div style="text-align: left">
-                        <button type="submit" class="btn btn-default">
-                            {{ __('Login') }}
-                        </button>
-                        @if (Route::has('admin.password.request'))
-                            <a class="reset_pass" href="{{ route('admin.password.request') }}">
-                                {{ __('Forgot Your Password?') }}
-                            </a>
-                        @endif
-                    </div>
-
-                    <div class="clearfix"></div>
-
-                    <div class="separator">
-
-                        <div class="clearfix"></div>
-                        <br/>
-
-                        <div>
-                            <h1>{{ config('app.name') }}</h1>
-                            <p>©{{ date('Y') }} All Rights Reserved.</p>
-                        </div>
-                    </div>
-                </form>
-            </section>
+            @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
-    </div>
-</div>
-</body>
-</html>
+        <div>
+            <input type="password" class="form-control @error('password') is-invalid @enderror" placeholder="Password"
+                   name="password" required
+                   autocomplete="current-password"/>
+
+            @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+        <div class="checkbox" style="text-align: left">
+            <label>
+                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
+            </label>
+        </div>
+        <div style="text-align: left">
+            <button type="submit" class="btn btn-default">
+                {{ __('Login') }}
+            </button>
+            @if (Route::has('admin.password.request'))
+                <a class="reset_pass" href="{{ route('admin.password.request') }}">
+                    {{ __('Forgot Your Password?') }}
+                </a>
+            @endif
+        </div>
+
+        <div class="clearfix"></div>
+
+        <div class="separator">
+
+            <div class="clearfix"></div>
+            <br/>
+
+            <div>
+                <h1>{{ config('app.name') }}</h1>
+                <p>©{{ date('Y') }} All Rights Reserved.</p>
+            </div>
+        </div>
+    </form>
+@endsection
--- resources/views/admin/auth/register.blade.php
+++ resources/views/admin/auth/register.blade.php
@@ -1,93 +1,64 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <!-- Meta, title, CSS, favicons, etc. -->
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('admin.auth.layout')
 
-    <title>{{ config('app.name') }} | Admin Register</title>
+@section('title', 'Admin Register')
 
-    <!-- Bootstrap -->
-    <link href="{{ asset('/assets/admin/vendors') }}/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Font Awesome -->
-    <link href="{{ asset('/assets/admin/vendors') }}/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <!-- NProgress -->
-    <link href="{{ asset('/assets/admin/vendors') }}/nprogress/nprogress.css" rel="stylesheet">
-    <!-- Animate.css -->
-    <link href="{{ asset('/assets/admin/vendors') }}/animate.css/animate.min.css" rel="stylesheet">
+@section('content')
+    <form method="POST" action="{{ route('admin.register') }}">
+        @csrf
+        <h1>Admin Register</h1>
+        <div>
+            <input type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Name"
+                   name="name" value="{{ old('name') }}" required autocomplete="name" autofocus/>
 
-    <!-- Custom Theme Style -->
-    <link href="{{ asset('/assets/admin/build') }}/css/custom.min.css" rel="stylesheet">
-</head>
-
-<body class="login">
-<div>
-    <div class="login_wrapper">
-        <div id="register" class="animate form login_form">
-            <section class="login_content">
-                <form method="POST" action="{{ route('admin.register') }}">
-                    @csrf
-                    <h1>Admin Register</h1>
-                    <div>
-                        <input type="text" class="form-control @error('name') is-invalid @enderror" placeholder="Name"
-                               name="name" value="{{ old('name') }}" required autocomplete="name" autofocus/>
-
-                        @error('name')
-                        <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                    <div>
-                        <input type="email" class="form-control @error('email') is-invalid @enderror"
-                               placeholder="Email" name="email" value="{{ old('email') }}" required
-                               autocomplete="email"/>
-
-                        @error('email')
-                        <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                    <div>
-                        <input type="password" class="form-control @error('password') is-invalid @enderror"
-                               placeholder="Password" name="password" required autocomplete="new-password"/>
-
-                        @error('password')
-                        <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                    <div>
-                        <input type="password" class="form-control"
-                               placeholder="Confirm Password" name="password_confirmation" required
-                               autocomplete="new-password"/>
-                    </div>
-                    <div style="text-align: left;">
-                        <button type="submit" class="btn btn-default">
-                            {{ __('Register') }}
-                        </button>
-                    </div>
-
-                    <div class="clearfix"></div>
-
-                    <div class="separator">
-
-                        <div class="clearfix"></div>
-                        <br/>
-
-                        <div>
-                            <h1>{{ config('app.name') }}</h1>
-                            <p>©{{ date('Y') }} All Rights Reserved.</p>
-                        </div>
-                    </div>
-                </form>
-            </section>
+            @error('name')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
         </div>
-    </div>
-</div>
-</body>
-</html>
+        <div>
+            <input type="email" class="form-control @error('email') is-invalid @enderror"
+                   placeholder="Email" name="email" value="{{ old('email') }}" required
+                   autocomplete="email"/>
+
+            @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+        <div>
+            <input type="password" class="form-control @error('password') is-invalid @enderror"
+                   placeholder="Password" name="password" required autocomplete="new-password"/>
+
+            @error('password')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+        <div>
+            <input type="password" class="form-control"
+                   placeholder="Confirm Password" name="password_confirmation" required
+                   autocomplete="new-password"/>
+        </div>
+        <div style="text-align: left;">
+            <button type="submit" class="btn btn-default">
+                {{ __('Register') }}
+            </button>
+        </div>
+
+        <div class="clearfix"></div>
+
+        <div class="separator">
+
+            <div class="clearfix"></div>
+            <br/>
+
+            <div>
+                <h1>{{ config('app.name') }}</h1>
+                <p>©{{ date('Y') }} All Rights Reserved.</p>
+            </div>
+        </div>
+    </form>
+@endsection

483da73ad0077ef15fd6f82c94fbf35ee3cc1920
unknown <rahamatj92@gmail.com>
2019-12-13 15:24:49 +0600

    admin reset password email set up

--- app/Admin.php
+++ app/Admin.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Notifications\Admin\ResetPassword as ResetPasswordNotification;
 
 class Admin extends Authenticatable
 {
@@ -38,4 +39,15 @@ class Admin extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Send the password reset notification.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPasswordNotification($token));
+    }
 }
